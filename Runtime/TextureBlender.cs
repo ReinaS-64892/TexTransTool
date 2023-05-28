5bf001233444b7a69ca837b8c5e25349ee4b8845
Reina_Sakiria <sasiki.64892@gmail.com>
2023-05-28 21:59:19 +0900

    Create TextureBlender

---
+++ Runtime/TextureBlender.cs
@@ -0,0 +1,97 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+#if VRC_BASE
+using VRC.SDKBase;
+#endif
+namespace Rs64.TexTransTool
+{
+    public class TextureBlender : TextureTransformer
+#if VRC_BASE
+    , IEditorOnly
+#endif
+    {
+        public Renderer TargetRenderer;
+        public int MaterialSelect = 0;
+        public Texture2D BlendTexture;
+        public BlendType BlendType = BlendType.Normal;
+        public string TargetPropatyName = "_MainTex";
+
+        public TextureBlenderDataContainer Container;
+
+        [SerializeField] protected bool _IsAppry = false;
+        public override bool IsAppry => _IsAppry;
+
+        public override bool IsPossibleAppry => TargetRenderer != null && BlendTexture != null;
+
+        public override bool IsPossibleCompile => IsPossibleAppry;
+
+        public override void Appry(MaterialDomain avatarMaterialDomain = null)
+        {
+            if (!IsPossibleAppry) return;
+            if (_IsAppry) return;
+            _IsAppry = true;
+            if (avatarMaterialDomain == null) avatarMaterialDomain = new MaterialDomain(new List<Renderer> { TargetRenderer });
+            ContainerCheck();
+
+            var DistMaterials = TargetRenderer.sharedMaterials;
+            (Material, Material) PeadMaterial;
+            Texture2D GeneretaTex;
+
+            var DistMat = DistMaterials[MaterialSelect];
+
+            var OldTex = DistMat.GetTexture(TargetPropatyName) as Texture2D;
+            if (OldTex == null) return;
+
+            var Newtex = TextureLayerUtil.BlendTextureUseComputeSheder(null, OldTex, BlendTexture, BlendType);
+            var SavedTex = AssetSaveHelper.SaveAsset(Newtex);
+
+            var NewMat = Instantiate<Material>(DistMat);
+            NewMat.SetTexture(TargetPropatyName, SavedTex);
+
+            PeadMaterial = (DistMat, NewMat);
+            GeneretaTex = SavedTex;
+
+
+            Container.BlendTexteres = GeneretaTex;
+            Container.GenereatMaterials = new List<MatPea> { new MatPea(DistMat, NewMat) };
+
+            avatarMaterialDomain.SetMaterial(PeadMaterial.Item1, PeadMaterial.Item2);
+        }
+
+        public override void Compile() { }
+
+        public override void Revart(MaterialDomain avatarMaterialDomain = null)
+        {
+            if (!_IsAppry) return;
+            _IsAppry = false;
+            if (avatarMaterialDomain == null) avatarMaterialDomain = new MaterialDomain(new List<Renderer> { TargetRenderer });
+
+            var MatsDict = MatPea.SwitchingdList(Container.GenereatMaterials);
+            avatarMaterialDomain.SetMaterials(MatPea.GeneratMatDict(MatsDict));
+        }
+
+        protected virtual void ContainerCheck()
+        {
+            if (Container == null) { Container = ScriptableObject.CreateInstance<TextureBlenderDataContainer>(); AssetSaveHelper.SaveAsset(Container); }
+        }
+    }
+
+    public class TextureBlenderDataContainer : TTDataContainer
+    {
+        [SerializeField] Texture2D _BlendTexteres;
+
+        public Texture2D BlendTexteres
+        {
+            set
+            {
+                if (_BlendTexteres != null) AssetSaveHelper.DeletAsset(_BlendTexteres);
+                _BlendTexteres = value;
+            }
+            get => _BlendTexteres;
+        }
+
+    }
+}
+#endif
