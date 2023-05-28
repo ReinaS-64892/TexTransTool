01fef3dd076e7eafd4d5b6466d216775741e8435
Reina_Sakiria <sasiki.64892@gmail.com>
2023-05-28 20:08:12 +0900

    add ifdef Containers

--- Runtime/Decal/DecalDataContainer.cs
+++ Runtime/Decal/DecalDataContainer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -32,3 +33,4 @@ namespace Rs64.TexTransTool.Decal
 
 
 }
+#endif
--- Runtime/TTDataContainer.cs
+++ Runtime/TTDataContainer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -113,3 +114,4 @@ namespace Rs64.TexTransTool
         }
     }
 }
+#endif
