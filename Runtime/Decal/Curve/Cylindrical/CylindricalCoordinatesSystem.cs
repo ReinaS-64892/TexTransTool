fcd49a9b11b08a93c739d4f759b06845ec874f9b
Reina_Sakiria <sasiki.64892@gmail.com>
2023-07-13 22:11:06 +0900

    add CCSSpaceAndFilter and etc...

--- Runtime/Decal/Curve/Cylindrical/CylindricalCoordinatesSystem.cs
+++ Runtime/Decal/Curve/Cylindrical/CylindricalCoordinatesSystem.cs
@@ -57,35 +57,132 @@ namespace Rs64.TexTransTool.Decal.Curve.Cylindrical
             return a;
         }
 
-        public List<Vector3> VartexsConvertCCS(List<Vector3> Vartexs, float OffSetAngle = 0)
+        public static float InversPosiNega(float angle)
         {
-            return Vartexs.Select(i =>
-            {
-                var CCSPoint = GetCCSPoint(i);
-                return new Vector3(CCSPoint.x, OffsetAngle(CCSPoint.y, OffSetAngle), CCSPoint.z);
-            }
-            ).ToList();
+            return angle + (Mathf.Sign(angle) * -360);
         }
 
-        public (List<Vector3>, List<Vector3>) VartexsConvertCCS(List<Vector3> Quad, List<Vector3> Varticals, bool IsAngeleOffset = true)
+        public List<Vector3> VartexsConvertCCS(IReadOnlyList<Vector3> Vartexs, float OffSetAngle = 0)
         {
-            float AngleOffset;
-            if (IsAngeleOffset)
+            List<Vector3> Convertvarts = new List<Vector3>();
+            foreach (var i in Vartexs)
             {
-                var NotOffsetQuad = VartexsConvertCCS(Quad, 0);
-                var MinValue = NotOffsetQuad[0].y;
-                NotOffsetQuad.ForEach(i => MinValue = Mathf.Max(MinValue, i.y));
-                AngleOffset = MinValue * -1;
-            }
-            else
-            {
-                AngleOffset = 0;
+                var CCSPoint = GetCCSPoint(i);
+                Convertvarts.Add(new Vector3(CCSPoint.x, OffsetAngle(CCSPoint.y, OffSetAngle), CCSPoint.z));
             }
 
-            var LoaclVarticals = VartexsConvertCCS(Varticals, AngleOffset);
-            var LoaclQuad = VartexsConvertCCS(Quad, AngleOffset);
-            //LoaclQuad.ForEach(i => Debug.Log( i.x + " " + i.y + " " + i.z) );
-            return (LoaclQuad, LoaclVarticals);
+            return Convertvarts;
+        }
+
+        public static void OffSetApply(List<Vector3> Varts, float Offset)
+        {
+            for (int i = 0; i < Varts.Count; i++)
+            {
+                Varts[i] = new Vector3(Varts[i].x, CylindricalCoordinatesSystem.OffsetAngle(Varts[i].y, Offset), Varts[i].z);
+            }
+        }
+
+
+        public static bool BorderOnPorygon(TraiangleIndex tri, List<Vector3> CCSvarts, float threshold = 150)
+        {
+            var CCStri = tri.GetTraiangle(CCSvarts);
+
+            var PosiCount = GetPositiveCount(tri, CCSvarts);
+
+            if (PosiCount == 0 || PosiCount == 3) return false;
+
+            var min = CCStri.Min(I => I.y);
+            var max = CCStri.Max(I => I.y);
+
+            return (max - min) > threshold;
+        }
+
+        public static int GetPositiveCount(TraiangleIndex Tri, List<Vector3> Varts)
+        {
+            var PositiveCount = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (Varts[Tri[i]].y > 0) PositiveCount += 1;
+            }
+
+            return PositiveCount;
+        }
+
+        public static void heightScaleFactor(List<Vector3> CCSVarts, float factor = 100)
+        {
+            for (int i = 0; i < CCSVarts.Count; i++)
+            {
+                CCSVarts[i] = new Vector3(CCSVarts[i].x * factor, CCSVarts[i].y, CCSVarts[i].z);
+            }
+        }
+
+    }
+
+
+    public class CCSSpace : DecalUtil.IConvertSpace
+    {
+        public CylindricalCoordinatesSystem CCS;
+        public List<Vector3> Quad;
+        public List<Vector3> CCSvarts;
+        public List<Vector3> CCSQuad;
+        public float Offset;
+        public List<Vector3> QuadNormalizedVarts;
+
+        public CCSSpace(CylindricalCoordinatesSystem CCS, List<Vector3> Quad)
+        {
+            this.CCS = CCS;
+            this.Quad = Quad;
+        }
+
+        public void Input(DecalUtil.MeshDatas Meshdatas)
+        {
+            var cCSQuad = CCS.VartexsConvertCCS(Quad);
+            var cCSVartexs = CCS.VartexsConvertCCS(Meshdatas.Varticals);
+            var offset = cCSQuad.Min(I => I.y) * -1;
+
+            CylindricalCoordinatesSystem.OffSetApply(cCSQuad, offset);
+            CylindricalCoordinatesSystem.OffSetApply(cCSVartexs, offset);
+
+            //円柱座標系での高さの値を大きくするとQuadNormalaizeの精度が上がる。
+            CylindricalCoordinatesSystem.heightScaleFactor(cCSVartexs);
+            CylindricalCoordinatesSystem.heightScaleFactor(cCSQuad);
+
+            Offset = offset;
+            var Normalized = DecalUtil.QuadNormaliz(cCSQuad.ConvertAll(i => (Vector2)i), cCSVartexs.ConvertAll(i => (Vector2)i));
+            QuadNormalizedVarts = Utils.ZipListVector3(Normalized, cCSVartexs.ConvertAll(i => i.z));
+
+            CCSvarts = cCSVartexs;
+            CCSQuad = cCSQuad;
+        }
+
+        public List<Vector2> OutPutUV()
+        {
+            var UV = new List<Vector2>(QuadNormalizedVarts.Capacity);
+            foreach (var i in QuadNormalizedVarts)
+            {
+                UV.Add(i);
+            }
+            return UV;
+        }
+    }
+
+    public class CCSFilter : DecalUtil.ITraiangleFilter<CCSSpace>
+    {
+        public float OutOfRangeOffset;
+
+        public CCSFilter(float outOfRangeOffset)
+        {
+            OutOfRangeOffset = outOfRangeOffset;
+        }
+
+        public List<TraiangleIndex> Filtering(CCSSpace Spase, List<TraiangleIndex> Traiangeles)
+        {
+            var Filters = new List<DecalUtil.Filtaring<CCSSpace>>();
+            Filters.Add((i, i2) => CylindricalCoordinatesSystem.BorderOnPorygon(i, i2.CCSvarts));
+            Filters.Add((i, i2) => DecalUtil.OutOfPorigonEdgeBase(i, i2.QuadNormalizedVarts, 1 + OutOfRangeOffset, 0 - OutOfRangeOffset, true));
+            var filtedTraiangle = DecalUtil.FiltaringTraiangle(Traiangeles, Spase, Filters);
+
+            return filtedTraiangle;
         }
     }
 }
--- Runtime/Decal/Curve/Cylindrical/CylindricalCurveDecal.cs
+++ Runtime/Decal/Curve/Cylindrical/CylindricalCurveDecal.cs
@@ -1,9 +1,6 @@
 #if UNITY_EDITOR
-using System;
-using System.Linq;
 using UnityEngine;
 using System.Collections.Generic;
-using static Rs64.TexTransTool.Decal.DecalUtil;
 #if VRC_BASE
 using VRC.SDKBase;
 #endif
@@ -57,7 +54,12 @@ namespace Rs64.TexTransTool.Decal.Curve.Cylindrical
             if (!IsPossibleCompile) return;
 
             Vector2? TexRenage = null;
-            if (IsTextureStreach) TexRenage = TextureStreathRenge;
+            TexWrapMode texWrapMode = TexWrapMode.NotWrap;
+            if (IsTextureStreach)
+            {
+                TexRenage = TextureStreathRenge;
+                texWrapMode = TexWrapMode.Stretch;
+            }
 
             var DictCompiledTextures = new List<Dictionary<Material, List<Texture2D>>>();
             int Count = 0;
@@ -78,14 +80,16 @@ namespace Rs64.TexTransTool.Decal.Curve.Cylindrical
 
                 foreach (var Renderer in TargetRenderers)
                 {
-                    DictCompiledTextures.Add(DecalUtil.CreatDecalTexture(Renderer, TargetDecalTexture,
-                    I => ComvartSpace(Quad, I),
-                    TargetPropatyName,
-                    TextureOutRenge: TexRenage,
-                    TrainagleFilters: GetFiltarings(),
-                    DefoaltPading: DefaultPading,
-                    TexWrapMode: TexWrapMode.Stretch
-                    ));
+                    var CCSspase = new CCSSpace(CylindricalCoordinatesSystem, Quad);
+                    var CCSfilter = new CCSFilter(OutOfRangeOffset);
+                    DictCompiledTextures.Add(DecalUtil.CreatDecalTexture(Renderer,
+                                                                         TargetDecalTexture,
+                                                                         CCSspase,
+                                                                         CCSfilter,
+                                                                         TargetPropatyName,
+                                                                         TextureOutRenge: TexRenage,
+                                                                         DefoaltPading: DefaultPading,
+                                                                         TexWrapMode: texWrapMode));
                 }
                 Count += 1;
             }
@@ -93,30 +97,6 @@ namespace Rs64.TexTransTool.Decal.Curve.Cylindrical
             var MatTexDict = ZipAndBlendTextures(DictCompiledTextures);
             var TextureList = Utils.GeneratTexturesList(Utils.GetMaterials(TargetRenderers), MatTexDict);
             SetContainer(TextureList);
-        }
-
-        private List<Vector3> ComvartSpace(List<Vector3> Quad, List<Vector3> Vartexs)
-        {
-            var LoaclPases = CylindricalCoordinatesSystem.VartexsConvertCCS(Quad, Vartexs, true);
-            var Normalaized = DecalUtil.QuadNormaliz(LoaclPases.Item1.ConvertAll(i => (Vector2)i), LoaclPases.Item2.ConvertAll(i => (Vector2)i));
-            return Utils.ZipListVector3(Normalaized, LoaclPases.Item2.ConvertAll(i => i.z));
-        }
-
-        public override List<DecalUtil.Filtaring> GetFiltarings()
-        {
-            List<DecalUtil.Filtaring> Filters = new List<DecalUtil.Filtaring>();
-
-            if (FiltedBackSide)
-            {
-                Filters.Add((i, i2) => DecalUtil.SideChek(i, i2, true));
-                Filters.Add((i, i2) => DecalUtil.OutOfPorigonEdgeBase(i, i2, 1 + OutOfRangeOffset, 0 - OutOfRangeOffset, true));
-            }
-            else
-            {
-                Filters.Add((i, i2) => DecalUtil.OutOfPorigonVartexBase(i, i2, 1 + OutOfRangeOffset, 0 - OutOfRangeOffset, true));
-            }
-
-            return Filters;
         }
     }
 }
