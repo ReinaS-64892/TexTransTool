eced7aaf4ee759c28796883a3b8593af48705f31
Reina_Sakiria <sasiki.64892@gmail.com>
2023-07-13 22:06:39 +0900

    add DebugUtils

---
+++ Runtime/DebugUtils.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System;
+using Rs64.TexTransTool;
+using UnityEngine;
+using System.Diagnostics;
+
+namespace Rs64.TexTransTool.DebugUtils
+{
+    public static class TraiangelDebug
+    {
+        public static string TraiangleToString(IEnumerable<TraiangleIndex> filtedTraiangle, IReadOnlyList<Vector3> debugvarts)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            foreach (var I in filtedTraiangle)
+            {
+                sb.Append($"{debugvarts[I[0]].x},{debugvarts[I[0]].y},{debugvarts[I[0]].z},");
+                sb.Append($"{debugvarts[I[1]].x},{debugvarts[I[1]].y},{debugvarts[I[1]].z},");
+                sb.Append($"{debugvarts[I[2]].x},{debugvarts[I[2]].y},{debugvarts[I[2]].z}\n");
+            }
+            return sb.ToString();
+        }
+    }
+
+    public class DebugTimer
+    {
+        Stopwatch _Stopwatch;
+        Stopwatch _StepStopwatch;
+
+        public DebugTimer()
+        {
+            _Stopwatch = new Stopwatch();
+            _StepStopwatch = new Stopwatch();
+            _Stopwatch.Start();
+            _StepStopwatch.Start();
+        }
+
+        public void Log(string messeg = "")
+        {
+            _StepStopwatch.Stop();
+            _Stopwatch.Stop();
+
+            ELtoLog(_StepStopwatch.Elapsed, messeg);
+
+            _StepStopwatch.Restart();
+            _Stopwatch.Start();
+        }
+
+        public void EndLog(string messeg = "")
+        {
+            Log(messeg);
+            _Stopwatch.Stop();
+            ELtoLog(_Stopwatch.Elapsed, "Total");
+        }
+
+        public static void ELtoLog(TimeSpan el, string messeg = "")
+        {
+            UnityEngine.Debug.Log($"{messeg} {el.Hours}h {el.Minutes}m {el.Seconds}s {el.Milliseconds}ms");
+        }
+    }
+}
