913fd17410cb65a465c91a32cc18842f6d8b3aba
Reina_Sakiria <sasiki.64892@gmail.com>
2023-07-14 19:25:21 +0900

    RW adjustment

--- Runtime/ComputeShaders/TransCompilerHelper.hlsl
+++ Runtime/ComputeShaders/TransCompilerHelper.hlsl
@@ -1,6 +1,6 @@
 #include "./TransHelper.hlsl"
 
-float4 GetColorBiliner(RWStructuredBuffer<float4> Souse, uint2 SourceTexSize,float2 GetPos) {
+float4 GetColorBiliner(StructuredBuffer<float4> Souse, uint2 SourceTexSize,float2 GetPos) {
   uint2 TexArrySize = SourceTexSize - uint2(1, 1);
   float2 pos = clamp(TexArrySize * GetPos,0,TexArrySize);
 
