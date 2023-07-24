e6814ada77b16959687c3711cbd5aa512ffb3c5c
Reina_Sakiria <sasiki.64892@gmail.com>
2023-07-24 15:31:18 +0900

    fix ResarchMaterials

--- Editor/TexturAtlas/AtlasTextureEditor.cs
+++ Editor/TexturAtlas/AtlasTextureEditor.cs
@@ -23,13 +23,13 @@ namespace Rs64.TexTransTool.TexturAtlas.Editor
 
             var S_TargetRoot = This_S_Object.FindProperty("TargetRoot");
 
-            var S_SelectRefarensMatList = This_S_Object.FindProperty("SelectRefarensMat").FindPropertyRelative("List");
+            var S_SelectRefarensMatList = This_S_Object.FindProperty("SelectRefarensMat");
             var S_MatSelectors = This_S_Object.FindProperty("MatSelectors");
 
             TextureTransformerEditor.ObjectReferencePorpty<GameObject>(S_TargetRoot, NewRoot =>
             {
                 Undo.RecordObject(ThisTarget, "AtlasTexture - TargetRoot");
-                ResarchMaterials(NewRoot, ThisTarget, S_SelectRefarensMatList, S_MatSelectors, ChannelCount);
+                ResarchMaterials(S_TargetRoot, NewRoot, S_SelectRefarensMatList, S_MatSelectors, ChannelCount);
                 This_S_Object.ApplyModifiedProperties();
             });
             if (S_TargetRoot.objectReferenceValue != null)
@@ -37,7 +37,7 @@ namespace Rs64.TexTransTool.TexturAtlas.Editor
                 if (GUILayout.Button("ResearchRenderas"))
                 {
                     Undo.RecordObject(ThisTarget, "AtlasTexture - SetTargetRoot");
-                    ResarchMaterials(ThisTarget.TargetRoot, ThisTarget, S_SelectRefarensMatList, S_MatSelectors, ChannelCount);
+                    ResarchMaterials(S_TargetRoot, ThisTarget.TargetRoot, S_SelectRefarensMatList, S_MatSelectors, ChannelCount);
                     This_S_Object.ApplyModifiedProperties();
                 }
                 if (GUILayout.Button("Automatic OffSet Setting"))
@@ -113,21 +113,28 @@ namespace Rs64.TexTransTool.TexturAtlas.Editor
             var S_SortingType = serializedProperty.FindPropertyRelative("SortingType");
             S_SortingType.enumValueIndex = 2;
         }
-        static void ResarchMaterials(GameObject NewRoot, AtlasTexture ThisTarget, SerializedProperty S_SelectRefarensMatList, SerializedProperty S_MatSelectors, int ChannelCount)
+        static void ResarchMaterials(SerializedProperty s_TargetRoot, GameObject NewRoot, SerializedProperty S_SelectRefarensMatList, SerializedProperty S_MatSelectors, int ChannelCount)
         {
-            ThisTarget.TargetRoot = NewRoot;
+            s_TargetRoot.objectReferenceValue = NewRoot;
             if (NewRoot == null) { return; }
-            ThisTarget.SelectRefarensMat = AtlasTexture.GetMaterials(ThisTarget.Renderers).ToList(false);
-            S_MatSelectors.arraySize = ThisTarget.SelectRefarensMat.Count;
+            var NewRefSouseMats = AtlasTexture.GetMaterials(AtlasTexture.FilterdRendarer(NewRoot.GetComponentsInChildren<Renderer>(true))).ToList(false);
+            S_SelectRefarensMatList.arraySize = NewRefSouseMats.Count;
+            for (var Index = 0; Index < NewRefSouseMats.Count; Index += 1)
+            {
+                var Mat = NewRefSouseMats[Index];
+                S_SelectRefarensMatList.GetArrayElementAtIndex(Index).objectReferenceValue = Mat;
+            }
 
-            for (var Index = 0; Index < ThisTarget.SelectRefarensMat.Count; Index += 1)
+            S_MatSelectors.arraySize = NewRefSouseMats.Count;
+
+            for (var Index = 0; Index < NewRefSouseMats.Count; Index += 1)
             {
                 var MatSelect = S_MatSelectors.GetArrayElementAtIndex(Index);
                 var SMat = MatSelect.FindPropertyRelative("Material");
                 var SChannel = MatSelect.FindPropertyRelative("AtlsChannel");
 
 
-                SMat.objectReferenceValue = ThisTarget.SelectRefarensMat[Index];
+                SMat.objectReferenceValue = NewRefSouseMats[Index];
                 SChannel.intValue = Mathf.Clamp(SChannel.intValue, 0, ChannelCount - 1);
             }
         }
