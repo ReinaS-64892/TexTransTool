aa2469466a9e3190a9fc21c24d0199afea75d7bb
Reina_Sakiria <sasiki.64892@gmail.com>
2023-08-29 22:15:33 +0900

    Remove: Random Test

--- TestCode/TransTextureTest.cs
+++ TestCode/TransTextureTest.cs
@@ -12,24 +12,23 @@ namespace net.rs64.TexTransTool.TestCode
         [TestCase(2)]
         [TestCase(3)]
         [TestCase(4)]
-        [TestCase(null)]
         public void TransTest(int? TestSeed)
         {
-            var sourcetex = new UnityEngine.Texture2D(512, 512, UnityEngine.TextureFormat.ARGB32, false);
+            var sourcesTex = new UnityEngine.Texture2D(512, 512, UnityEngine.TextureFormat.ARGB32, false);
 
-            var sourcetexpixsel = sourcetex.GetPixels();
-            var rundami = TestSeed.HasValue ? new System.Random(TestSeed.Value) : new System.Random();
+            var sourceTexPixel = sourcesTex.GetPixels();
+            var randomI = TestSeed.HasValue ? new System.Random(TestSeed.Value) : new System.Random();
 
-            for (var i = 0; sourcetexpixsel.Length > i; i += 1)
+            for (var i = 0; sourceTexPixel.Length > i; i += 1)
             {
-                sourcetexpixsel[i] = new UnityEngine.Color(rundami.Next(0, 255) / 255f, rundami.Next(0, 255) / 255f, rundami.Next(0, 255) / 255f, rundami.Next(0, 255) / 255f);
+                sourceTexPixel[i] = new UnityEngine.Color(randomI.Next(0, 255) / 255f, randomI.Next(0, 255) / 255f, randomI.Next(0, 255) / 255f, randomI.Next(0, 255) / 255f);
             }
-            sourcetex.SetPixels(sourcetexpixsel);
-            sourcetex.Apply();
+            sourcesTex.SetPixels(sourceTexPixel);
+            sourcesTex.Apply();
 
-            var targetrt = new UnityEngine.RenderTexture(512, 512, 0, UnityEngine.RenderTextureFormat.ARGB32);
+            var targetRt = new UnityEngine.RenderTexture(512, 512, 0, UnityEngine.RenderTextureFormat.ARGB32);
 
-            var trandata = new TransTexture.TransUVData(
+            var transData = new TransTexture.TransUVData(
                 new List<TraiangleIndex>()
                 {
                     new TraiangleIndex(0,1,2),
@@ -51,14 +50,14 @@ namespace net.rs64.TexTransTool.TestCode
                 }
             );
 
-            TransTexture.TransTextureToRenderTexture(targetrt, sourcetex, trandata, 0f, null);
-            var target2d = targetrt.CopyTexture2D();
+            TransTexture.TransTextureToRenderTexture(targetRt, sourcesTex, transData, 0f, null);
+            var target2d = targetRt.CopyTexture2D();
 
             for (var x = 0; x < 512; x += 1)
             {
                 for (var y = 0; y < 512; y += 1)
                 {
-                    var S = sourcetex.GetPixel(y, x);
+                    var S = sourcesTex.GetPixel(y, x);
                     var T = target2d.GetPixel(x, y);
 
                     Assert.That(S == T, Is.True);
