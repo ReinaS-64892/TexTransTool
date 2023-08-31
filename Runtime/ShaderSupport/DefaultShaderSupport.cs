5b11d17b801f289e2d39bcd6c3fd8beafbee1e83
Reina_Sakiria <sasiki.64892@gmail.com>
2023-08-31 17:20:48 +0900

    Remove: Unused Functions

--- Runtime/ShaderSupport/DefaultShaderSupport.cs
+++ Runtime/ShaderSupport/DefaultShaderSupport.cs
@@ -14,23 +14,6 @@ namespace net.rs64.TexTransTool.ShaderSupport
         public string ShaderName => "DefaultShader";
 
         public PropertyNameAndDisplayName[] GetPropertyNames => new PropertyNameAndDisplayName[] { new PropertyNameAndDisplayName("_MainTex", "MainTexture") };
-
-        public void AddRecord(Material material)
-        {
-        }
-        public void ClearRecord()
-        {
-        }
-
-        public List<PropAndTexture> GetPropertyAndTextures(Material material, bool IsGNTFMP = false)
-        {
-            if (material.HasProperty("_MainTex")){ return new List<PropAndTexture>() { new PropAndTexture("_MainTex", material.GetTexture("_MainTex")) }; }
-            else { return new List<PropAndTexture>(); }
-        }
-
-        public void MaterialCustomSetting(Material material)
-        {
-        }
     }
 }
 #endif
