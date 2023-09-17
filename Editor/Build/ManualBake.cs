a9bc876c5f1bf303df7bc9a608996f5c89d4a7e4
Reina_Sakiria <sasiki.64892@gmail.com>
2023-09-17 15:27:32 +0900

    Fix: MenuItemName Manual Bake Avatar

--- Editor/Build/ManualBake.cs
+++ Editor/Build/ManualBake.cs
@@ -7,10 +7,20 @@ namespace net.rs64.TexTransTool.Build
 {
     public static class ManualBake
     {
-        [MenuItem("Tools/TexTransTool/Manual Bake Avatar")]
-        public static void ManualBakeSelected()
+        [MenuItem("Tools/TexTransTool/TexTransTool Only Manual Bake Avatar")]
+        private static void ManualBakeSelected()
         {
             var targetAvatar = Selection.activeGameObject;
+            if (targetAvatar == null)
+            {
+                Debug.LogError("選択中の Avatar が存在しないため、Manual Bake Avatarを実行できません。");
+                return;
+            }
+            ManualBakeAvatar(targetAvatar);
+        }
+
+        public static void ManualBakeAvatar(GameObject targetAvatar)
+        {
             var duplicate = UnityEngine.Object.Instantiate(targetAvatar);
             duplicate.transform.position = new Vector3(duplicate.transform.position.x, duplicate.transform.position.y, duplicate.transform.position.z + 2);
             AvatarBuildUtils.ProcessAvatar(duplicate, null, false);
