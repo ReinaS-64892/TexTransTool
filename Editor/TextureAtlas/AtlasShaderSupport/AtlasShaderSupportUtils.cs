3938716782b06621ad5250b95e11dc575452c121
Reina_Sakiria <sasiki.64892@gmail.com>
2023-12-05 18:53:23 +0900

    Create: AtlasShaderRecorder

--- Editor/TextureAtlas/AtlasShaderSupport/AtlasShaderSupportUtils.cs
+++ Editor/TextureAtlas/AtlasShaderSupport/AtlasShaderSupportUtils.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using net.rs64.TexTransCore.TransTextureCore.Utils;
 using net.rs64.TexTransTool.ShaderSupport;
 using UnityEngine;
+using YamlDotNet.Core.Tokens;
 
 namespace net.rs64.TexTransTool.TextureAtlas
 {
@@ -67,6 +68,63 @@ namespace net.rs64.TexTransTool.TextureAtlas
         public IAtlasShaderSupport FindSupportI(Material material)
         {
             return _shaderSupports.Find(i => { return i.IsThisShader(material); });
+        }
+    }
+
+    public class AtlasShaderRecorder
+    {
+        Dictionary<string, PropRecord> propRecords = new Dictionary<string, PropRecord>();
+
+
+        //ここではテクスチャーは Texture2Dのみ扱う
+
+        //Color
+        //NormalizedFloat
+        //UnNormalizedFloat
+
+        public delegate bool ValueEqualityComparer<Value>(Value l, Value r);
+
+        public PropRecordAndValue<Value> AddRecord<Value>(Material mat, string texturePropertyName, Value additionalPropertyValue, ValueEqualityComparer<Value> equalityComparer)
+        where Value : struct
+        {
+            if (!mat.HasProperty(texturePropertyName)) { return null; }
+            if (propRecords.ContainsKey(texturePropertyName)) { propRecords[texturePropertyName] = new PropRecordAndValue<Value>(); }
+
+            var record = propRecords[texturePropertyName];
+            var vRecord = record as PropRecordAndValue<Value>;
+
+            var texture = mat.GetTexture(texturePropertyName) as Texture2D;
+            vRecord.ContainsTexture |= texture != null;
+
+            if (!vRecord.IsDifferenceValue.HasValue)
+            {
+                vRecord.IsDifferenceValue = false;
+                vRecord.RecordValue = additionalPropertyValue;
+            }
+            else
+            {
+                vRecord.IsDifferenceValue |= !equalityComparer.Invoke(vRecord.RecordValue, additionalPropertyValue);
+            }
+            return vRecord;
+        }
+
+        public PropRecordAndValue<Value> GetRecord<Value>(string texturePropertyName) where Value : struct
+        {
+            if (!propRecords.ContainsKey(texturePropertyName)) { return null; }
+            return propRecords[texturePropertyName] as PropRecordAndValue<Value>;
+        }
+
+
+        //テクスチャーのプロパティに付随する何かが値の違いがあるかやテクスチャが存在するかなどを記録する
+        public abstract class PropRecord
+        {
+            public bool ContainsTexture;
+            public bool? IsDifferenceValue;
+        }
+        public class PropRecordAndValue<Value> : PropRecord
+        where Value : struct
+        {
+            public Value RecordValue;
         }
     }
 }
