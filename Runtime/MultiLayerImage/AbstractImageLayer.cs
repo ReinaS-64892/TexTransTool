bd14c126e19acd129d97f9b67f1be02d2647a430
Reina_Sakiria <sasiki.64892@gmail.com>
2024-02-18 02:45:18 +0900

    feat: Clipping to PassThroughFolder

--- Runtime/MultiLayerImage/AbstractImageLayer.cs
+++ Runtime/MultiLayerImage/AbstractImageLayer.cs
@@ -18,17 +18,16 @@ namespace net.rs64.TexTransTool.MultiLayerImage
         public abstract void GetImage(RenderTexture renderTexture, IOriginTexture originTexture);
         internal override void EvaluateTexture(CanvasContext canvasContext)
         {
-            if (!Visible) { canvasContext.LayerCanvas.AddLayer(BlendLayer.Null(false, Clipping)); return; }
+            if (!Visible) { canvasContext.LayerCanvas.AddHiddenLayer(Clipping, false); return; }
 
             var canvasSize = canvasContext.CanvasSize;
             var rTex = RenderTexture.GetTemporary(canvasSize, canvasSize, 0); rTex.Clear();
 
             GetImage(rTex, canvasContext.TextureManager);
 
-            using (canvasContext.LayerCanvas.AlphaModScope(GetLayerAlphaMod(canvasContext)))
-            {
-                canvasContext.LayerCanvas.AddLayer(new(false, false, Clipping, rTex, BlendTypeKey));
-            }
+            var mask = GetLayerAlphaMod(canvasContext);
+            canvasContext.LayerCanvas.AddLayer(new(rTex, BlendTypeKey), mask, Clipping);
+
         }
 
 
