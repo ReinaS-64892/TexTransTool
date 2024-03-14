9c97f371b3d725ac0c33764f55cd4d0f519f5763
bd_ <bd_@nadena.dev>
2024-03-15 02:00:17 +0900

    opti: improve performance of IslandUtility
    Union-Findアルゴリズムを応用するのと、アロケーションを最小限にすることで自分のアバターで
    IslandToUVを役3msで処理できるようになりました。

--- TexTransCore/Island/IslandUtility.cs
+++ TexTransCore/Island/IslandUtility.cs
@@ -6,82 +6,169 @@ using System.Collections;
 using net.rs64.TexTransCore.TransTextureCore;
 using net.rs64.TexTransCore.TransTextureCore.Utils;
 using UnityEngine.Pool;
+using UnityEngine.Profiling;
 
 namespace net.rs64.TexTransCore.Island
 {
     internal static class IslandUtility
     {
+        /// <summary>
+        /// Union-FindアルゴリズムのためのNode Structureです。細かいアロケーションの負荷を避けるために、配列で管理する想定で、
+        /// ポインターではなくインデックスで親ノードを指定します。
+        ///
+        /// グループの代表でない限り、parentIndex以外の値は無視されます（古いデータが入る場合があります）
+        /// </summary>
+        internal struct VertNode
+        {
+            public int parentIndex;
+            
+            public (Vector2, Vector2) boundingBox;
+            
+            public int depth;
+            public int triCount;
+
+            public Island island;
+            
+            public VertNode(int i, Vector2 uv)
+            {
+                parentIndex = i;
+                boundingBox = (uv, uv);
+                depth = 0;
+                island = null;
+                triCount = 0;
+            }
+
+            /// <summary>
+            /// 指定したインデックスのノードのグループの代表ノードを調べる
+            /// </summary>
+            /// <param name="arr"></param>
+            /// <param name="index"></param>
+            /// <returns></returns>
+            public static int Find(VertNode[] arr, int index)
+            {
+                if (arr[index].parentIndex == index) return index;
+                
+                return arr[index].parentIndex = Find(arr, arr[index].parentIndex);
+            }
+
+            /// <summary>
+            /// 指定したふたつのノードを結合する
+            /// </summary>
+            /// <param name="arr"></param>
+            /// <param name="a"></param>
+            /// <param name="b"></param>
+            public static void Merge(VertNode[] arr, int a, int b)
+            {
+                a = Find(arr, a);
+                b = Find(arr, b);
+
+                if (a == b) return;
+                
+                if (arr[a].depth < arr[b].depth)
+                {
+                    (a, b) = (b, a);
+                }
+                
+                if (arr[a].depth == arr[b].depth) arr[a].depth++;
+                arr[b].parentIndex = a;
+                
+                arr[a].boundingBox = (Vector2.Min(arr[a].boundingBox.Item1, arr[b].boundingBox.Item1),
+                    Vector2.Max(arr[a].boundingBox.Item2, arr[b].boundingBox.Item2));
+                arr[a].triCount += arr[b].triCount;
+            }
+
+            /// <summary>
+            /// このグループに該当するIslandに三角面を追加します。Islandが存在しない場合は作成しislandListに追加します。
+            /// </summary>
+            /// <param name="idx"></param>
+            /// <param name="islandList"></param>
+            public void AddTriangle(TriangleIndex idx, List<Island> islandList)
+            {
+                if (island == null)
+                {
+                    islandList.Add(island = new Island());
+                    island.triangles.Capacity = triCount;
+                    
+                    var min = boundingBox.Item1;
+                    var max = boundingBox.Item2;
+                    
+                    island.Size = new Vector2(max.x - min.x, max.y - min.y);
+                    island.Pivot = min;
+                }
+                island.triangles.Add(idx);
+            }
+        }
+        
+        
         public static List<Island> UVtoIsland(List<TriangleIndex> triangles, List<Vector2> uv, IIslandCache caches = null)
         {
-            if (caches != null && caches.TryCache(uv, triangles, out List<Island> cacheHitIslands))
-                return cacheHitIslands;
-
-            // var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            Profiler.BeginSample("UVtoIsland");
             var islands = UVToIslandImpl(triangles, uv);
-            // Debug.Log($"UVtoIsland {triangles.Count} took {stopwatch.Elapsed}: {islands.Count}");
-
-            if (caches != null) { caches.AddCache(uv, triangles, islands); }
-
+            Profiler.EndSample();
+         
             return islands;
         }
 
         private static List<Island> UVToIslandImpl(IEnumerable<TriangleIndex> trianglesIn, List<Vector2> uvs)
         {
-            var triangles = new HashSet<TriangleIndex>(trianglesIn);
-            var trianglesByUv = new Dictionary<Vector2, HashSet<TriangleIndex>>(uvs.Count);
+            int uniqueUv = 0;
+            List<Vector2> indexToUv = new List<Vector2>(uvs.Count);
+            Dictionary<Vector2, int> uvToIndex = new Dictionary<Vector2, int>(uvs.Count);
+            List<int> inputVertToUniqueIndex = new List<int>(uvs.Count);
+            
+            var trianglesList = (trianglesIn as List<TriangleIndex>) ?? new List<TriangleIndex>(trianglesIn);
 
-            // initialize dictionary for less jumps
+            // 同一の位置にある頂点をまず調べて、共通のインデックスを割り当てます
+            Profiler.BeginSample("Preprocess vertices");
             foreach (var uv in uvs)
-                if (!trianglesByUv.ContainsKey(uv))
-                    trianglesByUv.Add(uv, new());
-
-            // collect all triangles by each triangle
-            foreach (var triangle in triangles)
             {
-                trianglesByUv[uvs[triangle.zero]].Add(triangle);
-                trianglesByUv[uvs[triangle.one]].Add(triangle);
-                trianglesByUv[uvs[triangle.two]].Add(triangle);
-            }
-
-            var islands = new List<Island>();
-
-            while (triangles.Count != 0)
-            {
-                var entryPoint = triangles.First();
-                triangles.Remove(entryPoint);
-                var trianglesOfIsland = new List<TriangleIndex> { entryPoint };
-
-                var proceedUvs = new HashSet<Vector2>();
-                var processUvQueue = new Queue<Vector2>();
-
-                if (proceedUvs.Add(uvs[entryPoint.zero])) processUvQueue.Enqueue(uvs[entryPoint.zero]);
-                if (proceedUvs.Add(uvs[entryPoint.one])) processUvQueue.Enqueue(uvs[entryPoint.one]);
-                if (proceedUvs.Add(uvs[entryPoint.two])) processUvQueue.Enqueue(uvs[entryPoint.two]);
-
-                while (processUvQueue.Count != 0)
+                if (!uvToIndex.TryGetValue(uv, out var uvVert))
                 {
-                    var uv = processUvQueue.Dequeue();
-                    var trianglesCandidate = trianglesByUv[uv];
-
-                    foreach (var triangle in trianglesCandidate)
-                    {
-                        // already the triangle is proceed
-                        if (!triangles.Remove(triangle)) continue;
-
-                        trianglesOfIsland.Add(triangle);
-                        if (proceedUvs.Add(uvs[triangle.zero])) processUvQueue.Enqueue(uvs[triangle.zero]);
-                        if (proceedUvs.Add(uvs[triangle.one])) processUvQueue.Enqueue(uvs[triangle.one]);
-                        if (proceedUvs.Add(uvs[triangle.two])) processUvQueue.Enqueue(uvs[triangle.two]);
-                    }
+                    uvVert = uvToIndex[uv] = uniqueUv++;
+                    indexToUv.Add(uv);
                 }
-
-                islands.Add(new(trianglesOfIsland));
+                
+                inputVertToUniqueIndex.Add(uvVert);
             }
+            Profiler.EndSample();
+            
+            VertNode[] nodes = new VertNode[uniqueUv];
 
-            foreach (var island in islands)
+            // Union-Find用のデータストラクチャーを初期化
+            Profiler.BeginSample("Init vertNodes");
+            for (int i = 0; i < uniqueUv; i++)
             {
-                island.BoxCalculation(uvs);
+                nodes[i] = new VertNode(i, indexToUv[i]);
             }
+            Profiler.EndSample();
+
+            Profiler.BeginSample("Merge vertices");
+            foreach (var tri in trianglesList)
+            {
+                int idx_a = inputVertToUniqueIndex[tri.zero];
+                int idx_b = inputVertToUniqueIndex[tri.one];
+                int idx_c = inputVertToUniqueIndex[tri.two];
+                
+                // 三角面に該当するノードを併合
+                VertNode.Merge(nodes, idx_a, idx_b);
+                VertNode.Merge(nodes, idx_b, idx_c);
+                
+                // 際アロケーションを避けるために三角面を数える
+                nodes[VertNode.Find(nodes, idx_a)].triCount++;
+            }
+            Profiler.EndSample();
+            
+            var islands = new List<Island>();
+            
+            // この時点で代表が決まっているので、三角を追加していきます。
+            Profiler.BeginSample("Add triangles to islands");
+            for (int i = 0; i < trianglesList.Count; i++)
+            {
+                int idx = inputVertToUniqueIndex[trianglesList[i].zero];
+
+                nodes[VertNode.Find(nodes, idx)].AddTriangle(trianglesList[i], islands);
+            }
+            Profiler.EndSample();
 
             return islands;
         }
