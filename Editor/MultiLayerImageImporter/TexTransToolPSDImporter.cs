2a37e77fb3e329e68a8861bdb9ce35d2bdaefdb7
Reina_Sakiria <sasiki.64892@gmail.com>
2024-03-21 14:55:30 +0900

    chore: move TexTransToolPSDImporterEditor.cs

--- Editor/MultiLayerImageImporter/TexTransToolPSDImporter.cs
+++ Editor/MultiLayerImageImporter/TexTransToolPSDImporter.cs
@@ -113,27 +113,6 @@ namespace net.rs64.TexTransTool.MultiLayerImage.Importer
 
     }
 
-    [CustomEditor(typeof(TexTransToolPSDImporter))]
-    class TexTransToolPSDImporterEditor : ScriptedImporterEditor
-    {
-        public override void OnInspectorGUI()
-        {
-            EditorGUILayout.HelpBox("TexTransToolPSDImporter" + " " + "Common:ExperimentalWarning".GetLocalize(), MessageType.Warning);
-
-            base.ApplyRevertGUI();
-        }
-        public override bool HasPreviewGUI() { return true; }
-        public override void DrawPreview(Rect previewArea)
-        {
-            var importer = target as TexTransToolPSDImporter;
-            var previewTex = AssetDatabase.LoadAllAssetsAtPath(importer.assetPath).FirstOrDefault(i => i.name == "TTT-CanvasPreviewResult") as Texture2D;
-            if (previewTex == null) { return; }
-
-            EditorGUI.DrawTextureTransparent(previewArea, previewTex, ScaleMode.ScaleToFit);
-        }
-
-    }
-
 
 
 
