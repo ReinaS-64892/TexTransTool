3ecd88a4f4e08f9b5a106989031841afcf747f62
Reina_Sakiria <sasiki.64892@gmail.com>
2024-04-07 21:57:28 +0900

    chore: initialize with LateInit

--- Editor/Utils/LateInitializeUtility.cs
+++ Editor/Utils/LateInitializeUtility.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using net.rs64.TexTransCore.BlendTexture;
 using net.rs64.TexTransCore.MipMap;
 using net.rs64.TexTransTool.MultiLayerImage;
@@ -28,6 +29,18 @@ namespace net.rs64.TexTransTool.Utils
             TexTransCore.TexTransCoreRuntime.Initialize();
             UnityEditor.EditorApplication.update += TexTransCore.TexTransCoreRuntime.Update.Invoke;
             MipMapUtility.MipMapShader = AssetDatabase.LoadAssetAtPath<ComputeShader>(AssetDatabase.GUIDToAssetPath("5f6d88c53276bb14eace10771023ae01"));
+
+            // TODO : これ何とかしないといけない
+            var atlasShaderSupportList = UnityEditor.AssetDatabase.GetAllAssetPaths()
+                         .Where(i => UnityEditor.AssetDatabase.GetMainAssetTypeAtPath(i) == typeof(TextureAtlas.AtlasScriptableObject.AtlasShaderSupportScriptableObject))
+                         .Select(i => UnityEditor.AssetDatabase.LoadAssetAtPath<TextureAtlas.AtlasScriptableObject.AtlasShaderSupportScriptableObject>(i))
+                         .ToList();
+
+            var _defaultSupport = ScriptableObject.CreateInstance<TextureAtlas.AtlasScriptableObject.AtlasShaderSupportScriptableObject>();
+            _defaultSupport.SupportedShaderComparer = new TextureAtlas.AtlasScriptableObject.AnythingShader();
+            _defaultSupport.AtlasTargetDefines = new() { new() { TexturePropertyName = "_MainTex", AtlasDefineConstraints = new TextureAtlas.AtlasScriptableObject.Anything() } };
+            atlasShaderSupportList.Add(_defaultSupport);
+            TextureAtlas.AtlasShaderSupportUtils.s_atlasShaderSupportList = atlasShaderSupportList;
 
         }
     }
