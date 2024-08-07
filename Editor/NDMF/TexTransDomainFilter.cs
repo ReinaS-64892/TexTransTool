0a043c94a4800c935b32175011612f009d901065
Reina_Sakiria <sasiki.64892@gmail.com>
2024-08-07 21:56:07 +0900

    fix forget observe disabled TexTransBehavior

--- Editor/NDMF/TexTransDomainFilter.cs
+++ Editor/NDMF/TexTransDomainFilter.cs
@@ -34,7 +34,6 @@ namespace net.rs64.TexTransTool.NDMF
         private ImmutableList<RenderGroup> QueryPreviewTarget(ComputeContext ctx)
         {
             var ttBehaviors = ctx.GetComponentsByType<TexTransBehavior>();
-            foreach (var ttb in ttBehaviors) { ctx.Observe(ttb); }
 
             var avatarGrouping = GroupingByAvatar(ttBehaviors);
             var allGroups = new List<RenderGroup>();
@@ -43,12 +42,14 @@ namespace net.rs64.TexTransTool.NDMF
                 var domainRoot = ag.Key;
                 var TexTransBehaviors = ag.Value;
 
+                foreach (var ttb in TexTransBehaviors) { ctx.ActiveInHierarchy(ttb.gameObject); }//先に ActiveInHierarchy をすべて見て無効化されているやつも見る
+
                 var domainRenderers = ctx.GetComponentsInChildren<Renderer>(domainRoot, true);
-                var phaseDict = AvatarBuildUtils.FindAtPhase(TexTransBehaviors);
+                var phaseDict = AvatarBuildUtils.FindAtPhase(TexTransBehaviors);//ここのなかで 無効化されたものはフィルタリングされる事がある
 
                 var (previewTargetBehavior, behaviorIndex) = GetFlattenBehaviorAndIndex(phaseDict);
 
-                var targetRendererGroup = GetTargetGrouping(ctx, domainRenderers, previewTargetBehavior);
+                var targetRendererGroup = GetTargetGrouping(ctx, domainRenderers, previewTargetBehavior);//ここでも無効化されたものはフィルタリングされる
                 var renderersGroup2behavior = GetRendererGrouping(behaviorIndex, targetRendererGroup);
 
                 allGroups.AddRange(renderersGroup2behavior.Select(i => RenderGroup.For(i.Key).WithData(i.Value)));
@@ -110,7 +111,7 @@ namespace net.rs64.TexTransTool.NDMF
             var targetRendererGroup = new Dictionary<TexTransBehavior, HashSet<Renderer>>();
             foreach (var ttb in previewTargetBehavior)
             {
-                if (!ctx.ActiveInHierarchy(ttb.gameObject)) { continue; }
+                if (ttb.ThisEnable is false) { continue; }
                 var modificationTargets = ttb.ModificationTargetRenderers(domainRenderers, (l, r) => l == r);
                 targetRendererGroup.Add(ttb, modificationTargets.ToHashSet());
             }
