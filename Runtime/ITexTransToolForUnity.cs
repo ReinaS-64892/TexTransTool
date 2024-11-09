da1b000a8951946bfa296ef46e7388ea3182d21a
Reina_Sakiria <rs@rs64.net>
2024-11-09 16:58:06 +0900

    feat: wrapping default implement

--- Runtime/ITexTransToolForUnity.cs
+++ Runtime/ITexTransToolForUnity.cs
@@ -1,6 +1,8 @@
+#nullable enable
 using System;
 using System.Collections.Generic;
 using net.rs64.TexTransCore;
+using net.rs64.TexTransCoreEngineForUnity;
 using net.rs64.TexTransCoreEngineForUnity.Utils;
 using net.rs64.TexTransTool.MultiLayerImage;
 using Unity.Collections;
@@ -16,8 +18,16 @@ namespace net.rs64.TexTransTool
         /// </summary>
         ITTBlendKey QueryBlendKey(string blendKeyName);
 
-        ITTDiskTexture Wrapping(Texture2D texture2D);
-        ITTDiskTexture Wrapping(TTTImportedImage texture2D);
+        // デフォルト実装は極力使わないように、パフォーマンスがごみカスだし画質もカスなので...
+        ITTDiskTexture Wrapping(Texture2D texture2D)
+        {
+            var rt = TTRt2.Get(texture2D.width, texture2D.height);
+            Graphics.Blit(texture2D, rt);
+            var discTex = new DefaultImplementRenderTexturedDiskTexture(UploadTexture(rt));
+            TTRt2.Rel(rt);
+            return discTex;
+        }
+        ITTDiskTexture Wrapping(TTTImportedImage texture2D) { return Wrapping(texture2D.PreviewTexture); }
 
         /// <summary>
         /// 基本的にパフォーマンスはカスであること前提なので使わない方向性で進めたいね
@@ -40,6 +50,26 @@ namespace net.rs64.TexTransTool
         }
 
         void DownloadTexture<T>(ITTRenderTexture renderTexture, TexTransCoreTextureFormat format, Span<T> dataDist) where T : unmanaged;
+    }
+
+    class DefaultImplementRenderTexturedDiskTexture : ITTDiskTexture
+    {
+        private ITTRenderTexture _renderTexture;
+
+        public DefaultImplementRenderTexturedDiskTexture(ITTRenderTexture renderTexture)
+        {
+            _renderTexture = renderTexture;
+        }
+        public int Width => _renderTexture.Width;
+
+        public int Hight => _renderTexture.Hight;
+
+        public string Name { get => _renderTexture.Name; set => _renderTexture.Name = value; }
+
+        public void Dispose()
+        {
+            _renderTexture.Dispose();
+        }
     }
 
 }
