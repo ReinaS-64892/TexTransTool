3b9231998584868b4a94cbfaf006209f492ce1dd
Reina_Sakiria <rs@rs64.net>
2024-11-26 21:39:07 +0900

    chore: add debug ReInitializeCanvasImportedImagePreview

--- Editor/MultiLayerImageImporter/CanvasImportedImagePreviewManager.cs
+++ Editor/MultiLayerImageImporter/CanvasImportedImagePreviewManager.cs
@@ -415,5 +415,13 @@ namespace net.rs64.TexTransTool.MultiLayerImage.Importer
             }
             s_guid2Images.Remove(guid);
         }
+
+        const string MenuPath = "Tools/" + TexTransBehavior.TTTName + "/Debug/ReInitializeCanvasImportedImagePreview";
+        [MenuItem(MenuPath)]
+        static void Reinitialize()
+        {
+            CanvasImportedImagePreviewInitialize();
+            ReleaseManager();
+        }
     }
 }
