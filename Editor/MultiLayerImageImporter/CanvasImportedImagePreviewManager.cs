0fb03cafea0892fdec8231f84f2a120f414fde57
Reina_Sakiria <rs@rs64.net>
2024-12-01 15:26:50 +0900

    pref: async loading from a file

--- Editor/MultiLayerImageImporter/CanvasImportedImagePreviewManager.cs
+++ Editor/MultiLayerImageImporter/CanvasImportedImagePreviewManager.cs
@@ -1,5 +1,5 @@
 #nullable enable
-// #undef CONTAINS_TTCE_WGPU
+#undef CONTAINS_TTCE_WGPU
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -64,9 +64,8 @@ namespace net.rs64.TexTransTool.MultiLayerImage.Importer
             PlaceHolderOrErrorTexture.Apply(true, true);
             PlaceHolderOrErrorTexture.filterMode = FilterMode.Point;
 
-#if CONTAINS_TTCE_WGPU
             EditorApplication.update += ForgetPreloadCollectOnesAndReleaseMemory;
-#endif
+
             AssemblyReloadEvents.beforeAssemblyReload += ReleaseManager;
             EditorApplication.playModeStateChanged += StateChangedEventLister;
 
@@ -108,14 +107,15 @@ namespace net.rs64.TexTransTool.MultiLayerImage.Importer
 #endif
         static void ReleaseManager()
         {
+            ForgetPreloadCollect();
 #if CONTAINS_TTCE_WGPU
             tryDeviceCreateLimiter = false;
-            ForgetPreloadCollect();
             s_ttceWgpuDevice?.Dispose();
             s_ttceWgpuDevice = null;
             EditorApplication.update -= ForgetPreloadCollectOnesAndReleaseMemory;
 #endif
             foreach (var tex in s_previewsDict.Values) { if (tex != null) { UnityEngine.Object.DestroyImmediate(tex); } }
+            s_previewsTask.Clear();
             s_previewsDict.Clear();
             s_guid2Images.Clear();
             if (PlaceHolderOrErrorTexture != null) { UnityEngine.Object.DestroyImmediate(PlaceHolderOrErrorTexture); }
@@ -134,49 +134,38 @@ namespace net.rs64.TexTransTool.MultiLayerImage.Importer
         static TTCEWgpuDevice? s_ttceWgpuDevice = null;
 
         static ShaderFinder.ShaderDictionary? s_shaderDictionary = null;
-        static Dictionary<TTTImportedImage, Task<Func<Texture2D>>> s_previewsTask = new();
 #endif
-
         public static Texture2D PlaceHolderOrErrorTexture { get; private set; } = null!;
 
+        static Dictionary<TTTImportedImage, Task<Func<Texture2D>>> s_previewsTask = new();
         static Dictionary<TTTImportedImage, Texture2D> s_previewsDict = new();
         static Dictionary<string, HashSet<TTTImportedImage>> s_guid2Images = new();
         public static Texture2D GetPreview(TTTImportedImage importedImage)
         {
-#if CONTAINS_TTCE_WGPU
             if (TryGetSyncCreated(importedImage, out var tex)) { return tex!; }
-#endif
             if (s_previewsDict.TryGetValue(importedImage, out var omMemoryPreviewTex)) { return omMemoryPreviewTex; }
             if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(importedImage, out var guid, out long fileID) is false) { return PlaceHolderOrErrorTexture; }
             var createOutputPath = Path.Combine(CachePath, guid, fileID.ToString());
 
-            if (File.Exists(createOutputPath))
-            {
-                return LoadFormFile(importedImage, guid, createOutputPath);
-            }
-            else
-            {
-                CreatePreviewImageWithCache(importedImage);
-#if CONTAINS_TTCE_WGPU
-                if (TryGetSyncCreated(importedImage, out var tex2)) { return tex2!; }// true だった場合は必ず値がある
-                else { return PlaceHolderOrErrorTexture; }//なんか失敗したらエラーのやつを返しとく
-#else
-                return s_previewsDict[importedImage];
-#endif
-            }
+            if (File.Exists(createOutputPath)) { LoadFormFile(importedImage, createOutputPath); }
+            else { CreatePreviewImageWithCache(importedImage); }
 
-#if CONTAINS_TTCE_WGPU
+            if (TryGetSyncCreated(importedImage, out var tex2)) { return tex2!; }// true だった場合は必ず値がある
+            if (s_previewsDict.TryGetValue(importedImage, out var texC)) { return texC; }
+            else { return PlaceHolderOrErrorTexture; }//なんか失敗したらエラーのやつを返しとく
+
+
             static bool TryGetSyncCreated(TTTImportedImage importedImage, out Texture2D? texture)
             {
                 if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(importedImage, out var guid, out long fileID) is false) { texture = null; return false; }
                 if (s_previewsTask.TryGetValue(importedImage, out var task) is false) { texture = null; return false; }
 
-
+                // DisplayProgressBar ってめちゃくちゃ重たいからやっぱなしで
+                // EditorUtility.DisplayProgressBar("CreatePreviewImage", "GetResult", 0.7f);
                 Profiler.BeginSample("GetResult", importedImage);
-                EditorUtility.DisplayProgressBar("CreatePreviewImage", "GetResult", 0.7f);
                 var tex2D = task.Result.Invoke();
                 Profiler.EndSample();
-                EditorUtility.ClearProgressBar();
+                // EditorUtility.ClearProgressBar();
 
                 s_previewsTask.Remove(importedImage);
                 texture = s_previewsDict[importedImage] = tex2D;
@@ -184,37 +173,49 @@ namespace net.rs64.TexTransTool.MultiLayerImage.Importer
                 else { s_guid2Images[guid] = new() { importedImage }; }
                 return true;
             }
-#endif
         }
 
-        private static Texture2D LoadFormFile(TTTImportedImage importedImage, string guid, string createOutputPath)
+        private static void LoadFormFile(TTTImportedImage importedImage, string path)
         {
+            if (s_previewsTask.TryGetValue(importedImage, out var _)) { return; }
+            if (s_previewsDict.TryGetValue(importedImage, out var _)) { return; }
+
             var needResizing = Math.Max(importedImage.CanvasDescription.Width, importedImage.CanvasDescription.Height) > 1024;
             var previewTex = new Texture2D(needResizing ? 1024 : importedImage.CanvasDescription.Width, needResizing ? 1024 : importedImage.CanvasDescription.Height, TextureFormat.BC7, false, true);
-            previewTex.LoadRawTextureData(File.ReadAllBytes(createOutputPath));
-            previewTex.Apply(true, true);
-            previewTex.name = "ImportedPreview-" + importedImage.name;
-            s_previewsDict[importedImage] = previewTex;
-            if (s_guid2Images.ContainsKey(guid)) { s_guid2Images[guid].Add(importedImage); }
-            else { s_guid2Images[guid] = new() { importedImage }; }
+            var dataNa = previewTex.GetRawTextureData<byte>();
 
-            return previewTex;
+            var task = Task.Run(() =>
+            {
+                Profiler.BeginSample("load from file", importedImage);
+                using var imageFile = File.OpenRead(path);
+                imageFile.Read(dataNa.AsSpan());
+                Profiler.EndSample();
+            });
+
+            var texFnTask = Task.Run(async () =>
+            {
+                await task.ConfigureAwait(false);
+
+                Func<Texture2D> tex2DResult = () =>
+                {
+                    previewTex.Apply(true, true);
+                    return previewTex;
+                };
+                return tex2DResult;
+            });
+
+            s_previewsTask[importedImage] = texFnTask;
         }
 
         public static void PreloadPreviewImage(TTTImportedImage importedImage, ITTImportedCanvasSource? canvasSource = null)
         {
-#if CONTAINS_TTCE_WGPU
             if (s_previewsTask.TryGetValue(importedImage, out var _)) { return; }
-#endif
             if (s_previewsDict.TryGetValue(importedImage, out var _)) { return; }
             if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(importedImage, out var guid, out long fileID) is false) { return; }
             var createOutputPath = Path.Combine(CachePath, guid, fileID.ToString());
 
-            if (File.Exists(createOutputPath) is false)
-            {
-                CreatePreviewImageWithCache(importedImage, canvasSource);
-            }
-            else { LoadFormFile(importedImage, guid, createOutputPath); }
+            if (File.Exists(createOutputPath) is false) { CreatePreviewImageWithCache(importedImage, canvasSource); }
+            else { LoadFormFile(importedImage, createOutputPath); }
         }
         public static void CreatePreviewImageWithCache(TTTImportedImage importedImage, ITTImportedCanvasSource? canvasSource = null)
         {
@@ -273,8 +274,9 @@ namespace net.rs64.TexTransTool.MultiLayerImage.Importer
                         var compressedData = previewTex.GetRawTextureData<byte>();
 
                         Profiler.BeginSample("Write to file");
-                        using (var stream = File.Open(createOutputPath, FileMode.CreateNew, FileAccess.Write))
-                            stream.Write(compressedData.AsSpan());
+                        if (Directory.Exists(Path.GetDirectoryName(createOutputPath)))
+                            using (var stream = File.Open(createOutputPath, FileMode.CreateNew, FileAccess.Write))
+                                stream.Write(compressedData.AsSpan());
                         Profiler.EndSample();
 
                         previewTex.Apply(true, true);
@@ -357,7 +359,6 @@ namespace net.rs64.TexTransTool.MultiLayerImage.Importer
             }
         }
 
-#if CONTAINS_TTCE_WGPU
         public static void ForgetPreloadCollectOnesAndReleaseMemory()
         {
             var image = s_previewsTask.Keys.FirstOrDefault();
@@ -384,7 +385,6 @@ namespace net.rs64.TexTransTool.MultiLayerImage.Importer
                 s_previewsTask.Clear();
             }
         }
-#endif
 
         public static void InvalidatesCacheAll()
         {
