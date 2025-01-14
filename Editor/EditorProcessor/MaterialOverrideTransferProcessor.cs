b82c55255c371fb594ea39b3e67d9bfb0fb2d446
tliks <aoft725@gmail.com>
2025-01-14 18:12:57 +0900

    feat: MaterialProperty and refactor

--- Editor/EditorProcessor/MaterialOverrideTransferProcessor.cs
+++ Editor/EditorProcessor/MaterialOverrideTransferProcessor.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using net.rs64.TexTransTool.Utils;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace net.rs64.TexTransTool.EditorProcessor
 {
@@ -14,71 +13,51 @@ namespace net.rs64.TexTransTool.EditorProcessor
         public void Process(TexTransCallEditorBehavior texTransCallEditorBehavior, IDomain domain)
         {
             var materialOverrideTransfer = texTransCallEditorBehavior as MaterialOverrideTransfer;
+            
+            if (materialOverrideTransfer.TargetMaterial == null) { TTTRuntimeLog.Info("MaterialOverrideTransfer:info:TargetNotSet"); return; }
+            if (materialOverrideTransfer.MaterialVariantSource == null) { TTTRuntimeLog.Info("MaterialOverrideTransfer:info:VariantNotSet"); return;}
 
-            var isValid = materialOverrideTransfer.TargetMaterial != null && materialOverrideTransfer.MaterialVariantSource != null;
-            if (materialOverrideTransfer.TargetMaterial == null) { TTTRuntimeLog.Info("MaterialOverrideTransfer:info:TargetNotSet"); }
-            if (materialOverrideTransfer.MaterialVariantSource == null) { TTTRuntimeLog.Info("MaterialOverrideTransfer:info:VariantNotSet"); }
-            if (isValid is false) { return; }
-
-            var materialVariantSource = materialOverrideTransfer.MaterialVariantSource;
             var mats = GetTargetMaterials(domain.EnumerateRenderer(), domain.OriginEqual, materialOverrideTransfer.TargetMaterial);
-
             if (mats.Any() is false) { TTTRuntimeLog.Info("MaterialOverrideTransfer:info:TargetNotFound"); return; }
-
-            var overridePropertyDict = new Dictionary<string, ShaderPropertyType>();
-            var shader = materialVariantSource.shader;
-            var pCount = shader.GetPropertyCount();
-            for (var i = 0; pCount > i; i += 1)
-            {
-                var propertyName = shader.GetPropertyName(i);
-                if (materialVariantSource.IsPropertyOverriden(propertyName)) { overridePropertyDict.Add(propertyName, shader.GetPropertyType(i)); }
-            }
 
             var materialSwapDict = new Dictionary<Material, Material>();
             foreach (var unEditableMat in mats)
             {
-                var mat = materialSwapDict[unEditableMat] = Material.Instantiate(unEditableMat);
-                foreach (var overrideProperty in overridePropertyDict)
-                {
-                    if (!mat.HasProperty(overrideProperty.Key)) { continue; }
-                    if (mat.shader.GetPropertyType(mat.shader.FindPropertyIndex(overrideProperty.Key)) != overrideProperty.Value) { continue; }
-
-                    switch (overrideProperty.Value)
-                    {
-                        case ShaderPropertyType.Texture:
-                            {
-                                mat.SetTexture(overrideProperty.Key, materialVariantSource.GetTexture(overrideProperty.Key));
-                                mat.SetTextureOffset(overrideProperty.Key, materialVariantSource.GetTextureOffset(overrideProperty.Key));
-                                mat.SetTextureScale(overrideProperty.Key, materialVariantSource.GetTextureScale(overrideProperty.Key));
-                                break;
-                            }
-                        case ShaderPropertyType.Color:
-                            {
-                                mat.SetColor(overrideProperty.Key, materialVariantSource.GetColor(overrideProperty.Key));
-                                break;
-                            }
-                        case ShaderPropertyType.Vector:
-                            {
-                                mat.SetVector(overrideProperty.Key, materialVariantSource.GetVector(overrideProperty.Key));
-                                break;
-                            }
-                        case ShaderPropertyType.Int:
-                            {
-                                mat.SetInt(overrideProperty.Key, materialVariantSource.GetInt(overrideProperty.Key));
-                                break;
-                            }
-                        case ShaderPropertyType.Float:
-                        case ShaderPropertyType.Range:
-                            {
-                                mat.SetFloat(overrideProperty.Key, materialVariantSource.GetFloat(overrideProperty.Key));
-                                break;
-                            }
-                    }
-                }
+                var mat = Material.Instantiate(unEditableMat);
+                var overrideProperties = GetOverrideProperties(materialOverrideTransfer.MaterialVariantSource);
+                SetProperties(mat, overrideProperties);
+                materialSwapDict[unEditableMat] = mat;
             }
-
             domain.ReplaceMaterials(materialSwapDict);
         }
+
+        public static void SetProperties(Material mat, IEnumerable<MaterialProperty> materialProperties)
+        {
+            foreach (var materialProperty in materialProperties)
+            {
+                materialProperty.Set(mat);
+            }
+        }
+
+        public static IEnumerable<MaterialProperty> GetOverrideProperties(Material variant)
+        {
+            if (variant == null) yield break;
+
+            var shader = variant.shader;
+            var pCount = shader.GetPropertyCount();
+            for (var i = 0; pCount > i; i += 1)
+            {
+                var propertyName = shader.GetPropertyName(i);
+                var propertyType = shader.GetPropertyType(i);
+
+                if (!variant.IsPropertyOverriden(propertyName)) continue;
+
+                if (!MaterialProperty.TryGet(variant, propertyName, propertyType, out var property)) continue;
+
+                yield return property;
+            }
+        }
+
 
         private static IEnumerable<Material> GetTargetMaterials(IEnumerable<Renderer> domainRenderer, OriginEqual originEqual, Material target)
         {
