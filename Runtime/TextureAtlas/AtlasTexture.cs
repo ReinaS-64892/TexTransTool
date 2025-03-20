3d320a38fcd36ae09c2288d3641f2697277a0f6e
Reina_Sakiria <rs@rs64.net>
2025-03-20 22:35:06 +0900

    chore: move TransMoveRectangle

--- Runtime/TextureAtlas/AtlasTexture.cs
+++ Runtime/TextureAtlas/AtlasTexture.cs
@@ -12,6 +12,7 @@ using net.rs64.TexTransCore;
 using net.rs64.TexTransCore.UVIsland;
 using System.Runtime.InteropServices;
 using System.Buffers.Binary;
+using net.rs64.TexTransCore.AtlasTexture;
 
 namespace net.rs64.TexTransTool.TextureAtlas
 {
@@ -188,8 +189,7 @@ namespace net.rs64.TexTransTool.TextureAtlas
                         var drawTargetMovedVirtualIslands = drawTargetSourceVirtualIslands.Select(i => source2MovedVirtualIsland[i]).ToArray();
 
 
-                        TransMoveRectangle(
-                            engine
+                        AtlasingUtility.TransMoveRectangle(engine
                             , targetRT
                             , sourceRenderTexture
                             , drawTargetSourceVirtualIslands
@@ -479,124 +479,6 @@ namespace net.rs64.TexTransTool.TextureAtlas
             // foreach (var atlasMeshHolder in atlasData.Meshes) { UnityEngine.Object.DestroyImmediate(atlasMeshHolder.NormalizedMesh); }
         }
 
-        [StructLayout(LayoutKind.Sequential, Pack = 4)]
-        struct InputRect
-        {
-            public float SourcePositionX;
-            public float SourcePositionY;
-            public float SourceSizeX;
-            public float SourceSizeY;
-
-            public float SourceRotation;
-            public float AlimentPadding11;
-            public float AlimentPadding12;
-            public float AlimentPadding13;
-
-
-            public float TargetPositionX;
-            public float TargetPositionY;
-
-            public float TargetSizeX;
-            public float TargetSizeY;
-
-            public float TargetRotation;
-            public float AlimentPadding21;
-            public float AlimentPadding22;
-            public float AlimentPadding23;
-        }
-        private static void TransMoveRectangle(
-            ITexTransToolForUnity engine
-            , ITTRenderTexture targetRT
-            , ITTRenderTexture sourceTexture
-            , IslandTransform[] drawTargetSourceVirtualIslands
-            , IslandTransform[] drawTargetMovedVirtualIslands
-            , float islandPadding
-        )
-        {
-
-            var atlasHeightScale = targetRT.Hight / (float)targetRT.Width;
-            var inputRecs = new InputRect[drawTargetSourceVirtualIslands.Length];
-            for (var i = 0; inputRecs.Length > i; i += 1)
-            {
-                var sourceRect = drawTargetSourceVirtualIslands[i];
-                var targetRect = drawTargetMovedVirtualIslands[i];
-
-                inputRecs[i] = new InputRect
-                {
-                    SourcePositionX = sourceRect.Position.X,
-                    SourcePositionY = sourceRect.Position.Y,
-                    SourceSizeX = sourceRect.Size.X,
-                    SourceSizeY = sourceRect.Size.Y,
-                    SourceRotation = sourceRect.Rotation,
-
-                    TargetPositionX = targetRect.Position.X,
-                    TargetPositionY = targetRect.Position.Y,
-                    TargetSizeX = targetRect.Size.X,
-                    TargetSizeY = targetRect.Size.Y,
-                    TargetRotation = targetRect.Rotation,
-                };
-            }
-            Span<byte> mappingConstantBuffer = stackalloc byte[32];
-            BitConverter.TryWriteBytes(mappingConstantBuffer.Slice(0, 4), (uint)targetRT.Width);
-            BitConverter.TryWriteBytes(mappingConstantBuffer.Slice(4, 4), (uint)targetRT.Hight);
-            BitConverter.TryWriteBytes(mappingConstantBuffer.Slice(8, 4), (uint)sourceTexture.Width);
-            BitConverter.TryWriteBytes(mappingConstantBuffer.Slice(12, 4), (uint)sourceTexture.Hight);
-
-            BitConverter.TryWriteBytes(mappingConstantBuffer.Slice(16, 4), islandPadding);
-            BitConverter.TryWriteBytes(mappingConstantBuffer.Slice(20, 4), atlasHeightScale);
-            BitConverter.TryWriteBytes(mappingConstantBuffer.Slice(24, 4), 0.0f);
-            BitConverter.TryWriteBytes(mappingConstantBuffer.Slice(28, 4), 0.0f);
-
-
-            using var inputRectBuffer = engine.UploadStorageBuffer<InputRect>(inputRecs);
-            using var transMap = engine.CreateRenderTexture(targetRT.Width, targetRT.Hight, TexTransCoreTextureChannel.RG);
-            using var scalingMap = engine.CreateRenderTexture(targetRT.Width, targetRT.Hight, TexTransCoreTextureChannel.R);
-            using var isWriteMap = engine.CreateRenderTexture(targetRT.Width, targetRT.Hight, TexTransCoreTextureChannel.R);
-
-            using (var mappingHandler = engine.GetComputeHandler(engine.GetExKeyQuery<IAtlasComputeKey>().RectangleTransMapping))
-            {
-                var gvBufID = mappingHandler.NameToID("gv");
-                var transMapID = mappingHandler.NameToID("TransMap");
-                var scalingMapID = mappingHandler.NameToID("ScalingMap");
-                var writeMapID = mappingHandler.NameToID("WriteMap");
-                var mappingRectBufID = mappingHandler.NameToID("MappingRect");
-
-                mappingHandler.SetStorageBuffer(mappingRectBufID, inputRectBuffer);
-                mappingHandler.UploadConstantsBuffer<byte>(gvBufID, mappingConstantBuffer);
-
-                mappingHandler.SetTexture(transMapID, transMap);
-                mappingHandler.SetTexture(scalingMapID, scalingMap);
-                mappingHandler.SetTexture(writeMapID, isWriteMap);
-
-                mappingHandler.Dispatch((uint)inputRecs.Length, 1, 1);
-            }
-
-            Span<uint> readTextureParm = stackalloc uint[4];
-            readTextureParm[0] = (uint)sourceTexture.Width;
-            readTextureParm[1] = (uint)sourceTexture.Hight;
-            readTextureParm[2] = readTextureParm[3] = 0;
-
-            using (var samplerHandler = engine.GetComputeHandler(engine.GetExKeyQuery<IAtlasSamplerComputeKey>().AtlasSamplerKey[engine.StandardComputeKey.DefaultSampler]))
-            {
-                var readTextureParmBufID = samplerHandler.NameToID("ReadTextureParm");
-                var readTexID = samplerHandler.NameToID("ReadTex");
-
-                var transMapID = samplerHandler.NameToID("TransMap");
-                var scalingMapID = samplerHandler.NameToID("ScalingMap");
-                var isWriteMapID = samplerHandler.NameToID("WriteMap");
-                var targetTexID = samplerHandler.NameToID("TargetTex");
-
-                samplerHandler.UploadConstantsBuffer<uint>(readTextureParmBufID, readTextureParm);
-                samplerHandler.SetTexture(readTexID, sourceTexture);
-
-                samplerHandler.SetTexture(transMapID, transMap);
-                samplerHandler.SetTexture(scalingMapID, scalingMap);
-                samplerHandler.SetTexture(isWriteMapID, isWriteMap);
-                samplerHandler.SetTexture(targetTexID, targetRT);
-
-                samplerHandler.DispatchWithTextureSize(targetRT);
-            }
-        }
         private static int GetNormalizedMinHeightSize(int atlasTextureSize, float height)
         {
             switch (height)
