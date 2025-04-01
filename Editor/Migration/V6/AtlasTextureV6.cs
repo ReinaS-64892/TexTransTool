fedd14dca76ec08cd967f27eb7c15d90b2501273
Reina_Sakiria <rs@rs64.net>
2025-04-01 20:55:26 +0900

    feat: Migration AtlasTexture V6 TO V7

---
+++ Editor/Migration/V6/AtlasTextureV6.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using net.rs64.TexTransTool.TextureAtlas;
+using net.rs64.TexTransTool.TextureAtlas.IslandSizePriorityTuner;
+using UnityEditor;
+using UnityEngine;
+
+namespace net.rs64.TexTransTool.Migration.V6
+{
+    [Obsolete]
+    internal static class AtlasTextureV6
+    {
+        public static void MigrationAtlasTextureV6ToV7(AtlasTexture atlasTexture)
+        {
+            if (atlasTexture == null) { Debug.LogWarning("マイグレーションターゲットが存在しません。"); return; }
+            if (atlasTexture is ITexTransToolTag TTTag && TTTag.SaveDataVersion > 7) { Debug.Log(atlasTexture.name + " AtlasTexture : マイグレーション不可能なバージョンです。"); return; }
+
+
+            atlasTexture.AtlasTargetMaterials = atlasTexture.SelectMatList.Select(i => i.Material).ToList();
+            atlasTexture.IslandSizePriorityTuner = atlasTexture.SelectMatList.Select(i =>
+                new SetFromMaterial()
+                {
+                    Materials = new() { i.Material },
+                    PriorityValue = i.MaterialFineTuningValue
+                }
+            ).ToList<IIslandSizePriorityTuner>();
+
+#if CONTAINS_LNU
+            if (atlasTexture.AtlasSetting.PropertyBakeSetting == PropertyBakeSetting.NotBake)
+            {
+                if (atlasTexture.MigrationTemporarylilToonMaterialNormalizerReference != null)
+                {
+                    atlasTexture.MigrationTemporarylilToonMaterialNormalizerReference.enabled = false;
+                }
+            }
+            else
+            {
+                if (atlasTexture.MigrationTemporarylilToonMaterialNormalizerReference == null)
+                    if (lilToonNDMFUtility.lilToonMaterialNormalizerPublicAPI.TryAddComponent(atlasTexture.gameObject, out var lnuMn))
+                        atlasTexture.MigrationTemporarylilToonMaterialNormalizerReference = lnuMn as Behaviour;
+
+                if (atlasTexture.MigrationTemporarylilToonMaterialNormalizerReference != null)
+                {
+                    var lnuMN = atlasTexture.MigrationTemporarylilToonMaterialNormalizerReference;
+                    lnuMN.enabled = true;
+
+                    lilToonNDMFUtility.lilToonMaterialNormalizerPublicAPI.TrySetTargetMaterials(lnuMN, atlasTexture.AtlasTargetMaterials);
+                    if (atlasTexture.AtlasSetting.PropertyBakeSetting is PropertyBakeSetting.Bake)
+                        lilToonNDMFUtility.lilToonMaterialNormalizerPublicAPI.TrySetTargetDetectionOfTextureContains(lnuMN);
+                    else
+                        lilToonNDMFUtility.lilToonMaterialNormalizerPublicAPI.TrySetTargetDetectionOfAll(lnuMN);
+                }
+            }
+#endif
+
+
+
+
+            EditorUtility.SetDirty(atlasTexture);
+            MigrationUtility.SetSaveDataVersion(atlasTexture, 7);
+        }
+    }
+}
