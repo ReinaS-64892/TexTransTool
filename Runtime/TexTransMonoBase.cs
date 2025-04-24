8816b514aff523a6c1f5a22393a84cc3743797e3
Reina_Sakiria <rs@rs64.net>
2025-04-24 23:27:27 +0900

    feat: TexTransToolStablePublicAPIAttribute

--- Runtime/TexTransMonoBase.cs
+++ Runtime/TexTransMonoBase.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 
 namespace net.rs64.TexTransTool
 {
+    /*
+    TexTransMonoBase を継承した TTT のコンポーネントは一応ほかツールが AddComponent などの連携をできるようにすることを完全に禁止したくないので Public になっています。
+
+    ただし、
+
+    - **API に対する要望は基本的に受け付けない可能性があり、削除された物を復活させてほしいなどの要望はすべて受け付けません。**
+    - 実験的なコンポーネント(ITexTransToolStableComponent が実装されていないコンポーネント)については一切保証しません。
+    - TTT の実験的ではない(ITexTransToolStableComponent が実装されている)コンポーネントのフィールドに対しては、同一のマイナーバージョン間でのみ保証します。（つまり、マイナーが変わるときには破壊される可能性があります。）
+      - v0.x の間は同一マイナーバージョンでも保証されません。（つまり v0.x の間はパッチバージョンでも破壊される可能性があります。）
+    - .asmdef などで適切にエラーにならないように使用してください。
+
+    なお TexTransTool にはコンポーネント以外にも Public されている関数や API などが存在しますが、それらははすべて保証されません。
+    それらに触れる場合はパッチバージョンレベルで asmdef で指定してください。
+
+    ですが、[TexTransToolStablePublicAPI] が付与された Public API や Public な関数については、特別に、メジャーバージョンが同一な間は保証されます。
+    (つまり、メジャーバージョンが変わる場合は破壊される可能性が在ります。ただし、v0.x.x は v1.x.x と同一のものとみなし、v0.x.x にて追加された [TexTransToolStablePublicAPI] の API は v1.x.x の間まで保証します。)
+
+
+    **もし、これら API 関係で TexTransTool および Reina_Sakiria に不利益を及ばせるものが見受けられた場合、[TexTransToolStablePublicAPI] を除いたすべての API が予告なく internal に戻る可能性があります。**
+    */
     [ExecuteInEditMode]
     public abstract class TexTransMonoBase : MonoBehaviour, ITexTransToolTag
     {
@@ -31,5 +51,10 @@ namespace net.rs64.TexTransTool
     {
         public static event Action<TexTransMonoBase>? OnDestroy;
         public static void DestroyThis(TexTransMonoBase destroy) => OnDestroy?.Invoke(destroy);
+    }
+
+    [System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = false)]
+    sealed class TexTransToolStablePublicAPIAttribute : System.Attribute
+    {
     }
 }
