b459e8ca2d4dd050d3eb998d09b63979f20406a0
Reina_Sakiria <rs@rs64.net>
2025-05-17 19:59:21 +0900

    chore: remove TTTException

--- Runtime/Exception/TTTNotExecutable.cs
+++ Runtime/Exception/TTTNotExecutable.cs
@@ -1,7 +1,9 @@
+using net.rs64.TexTransCore;
+
 namespace net.rs64.TexTransTool
 {
     [System.Serializable]
-    public class TTTNotExecutable : TTTException
+    public class TTTNotExecutable : TTException
     {
         public TTTNotExecutable(params object[] additionalMessage) : base("Not Executable", additionalMessage) { }
         public TTTNotExecutable(string message, params object[] additionalMessage) : base(message, additionalMessage) { }
