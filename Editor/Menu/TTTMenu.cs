c335a20356327f67f8acd7a2d525c5e06d1f0f22
Reina_Sakiria <rs@rs64.net>
2025-08-25 17:48:55 +0900

    chore: global , local menu

--- Editor/Menu/TTTMenu.cs
+++ Editor/Menu/TTTMenu.cs
@@ -1,5 +1,4 @@
 #nullable enable
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
@@ -11,29 +10,34 @@ namespace net.rs64.TexTransTool
     [EditorWindowTitle(title = "TTT Menu")]
     internal sealed class TTTMenu : EditorWindow
     {
-
         [MenuItem("Tools/TexTransTool/Menu")]
-        public static void ShowWindow(){ ShowWindow(null); }
-        public static void ShowWindow(Type? openMenu = null)
+        public static void ShowWindow()
         {
-            var window = GetWindow<TTTMenu>();
-            if (openMenu is not null) window.ShowMenu(openMenu);
+            GetWindow<TTTMenu>();
         }
-
-        private void ShowMenu(Type openMenu)
+        static List<ITTTMenuWindow> _globalMenus = new() { new TTTConfigMenu() };
+        List<ITTTMenuWindow>? _menus;
+        public void InitializeMenuList(IEnumerable<ITTTMenuWindow>? insert = null)
         {
-            if (utilityPanel is null) { return; }
-
-            var menu = _menus.FirstOrDefault(m => m.GetType() == openMenu);
-            if (menu is null) { return; }
-
-            utilityPanel.hierarchy.Clear();
-            utilityPanel.hierarchy.Add(menu.CreateGUI());
+            if (insert is null)
+            {
+                if (_menus is null)
+                {
+                    _menus = _globalMenus.ToList();
+                }
+            }
+            else
+            {
+                if (_menus is null)
+                {
+                    _menus = insert.Concat(_globalMenus).ToList();
+                }
+                else
+                {
+                    _menus.InsertRange(0, insert);
+                }
+            }
         }
-
-        static List<ITTTMenuWindow> _menus = new() { new TTTConfigMenu() };
-        private VisualElement? utilityPanel;
-
         public void CreateGUI()
         {
             rootVisualElement.Clear();
@@ -43,18 +47,17 @@ namespace net.rs64.TexTransTool
             rootVisualElement.hierarchy.Add(root);
 
             var utilitiesScrollView = new ScrollView();
-            utilitiesScrollView.style.flexShrink = 0;
             var scrollViewContainer = utilitiesScrollView.Q<VisualElement>("unity-content-container");
             scrollViewContainer.style.flexDirection = FlexDirection.Row;
             root.hierarchy.Add(utilitiesScrollView);
 
-            utilityPanel = new VisualElement();
+            var utilityPanel = new VisualElement();
             utilityPanel.style.width = Length.Percent(100);
             root.hierarchy.Add(utilityPanel);
 
-
-            utilityPanel.hierarchy.Add(_menus.First().CreateGUI());
-            foreach (var menu in _menus)
+            InitializeMenuList();
+            utilityPanel.hierarchy.Add(_menus!.First().CreateGUI());
+            foreach (var menu in _menus!)
             {
                 var button = new Button();
                 button.text = menu.MenuName;
@@ -80,7 +83,7 @@ namespace net.rs64.TexTransTool
 
         internal static void RegisterMenu(ITTTMenuWindow menuWindow)
         {
-            _menus.Add(menuWindow);
+            _globalMenus.Add(menuWindow);
         }
     }
 }
