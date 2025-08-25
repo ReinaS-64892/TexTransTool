6a0c9c72cb9988e84be674ea4f0fd688f8108f59
Reina_Sakiria <rs@rs64.net>
2025-08-25 16:40:41 +0900

    feat: UIElements on TTTMenu

--- Editor/Menu/TTTMenu.cs
+++ Editor/Menu/TTTMenu.cs
@@ -1,7 +1,9 @@
 #nullable enable
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace net.rs64.TexTransTool
 {
@@ -14,26 +16,48 @@ namespace net.rs64.TexTransTool
         {
             GetWindow<TTTMenu>();
         }
-
-        int _state = 0;
         static List<ITTTMenuWindow> _menus = new() { new TTTConfigMenu() };
-
-        public void OnGUI()
+        public void CreateGUI()
         {
-            EditorGUIUtility.labelWidth = 256f;
-            using (new EditorGUILayout.HorizontalScope())
-                for (var i = 0; _menus.Count > i; i += 1)
-                {
-                    if (GUILayout.Button(_menus[i].MenuName)) { _state = i; }
-                }
+            rootVisualElement.Clear();
 
-            _menus[_state].OnGUI();
+            var root = new VisualElement();
+            root.style.flexDirection = FlexDirection.Column;
+            rootVisualElement.hierarchy.Add(root);
+
+            var utilitiesScrollView = new ScrollView();
+            var scrollViewContainer = utilitiesScrollView.Q<VisualElement>("unity-content-container");
+            scrollViewContainer.style.flexDirection = FlexDirection.Row;
+            root.hierarchy.Add(utilitiesScrollView);
+
+            var utilityPanel = new VisualElement();
+            utilityPanel.style.width = Length.Percent(100);
+            root.hierarchy.Add(utilityPanel);
+
+
+            utilityPanel.hierarchy.Add(_menus.First().CreateGUI());
+            foreach (var menu in _menus)
+            {
+                var button = new Button();
+                button.text = menu.MenuName;
+                button.clicked += () =>
+                {
+                    utilityPanel.hierarchy.Clear();
+                    utilityPanel.hierarchy.Add(menu.CreateGUI());
+                };
+
+                scrollViewContainer.hierarchy.Add(button);
+            }
         }
 
         internal interface ITTTMenuWindow
         {
             string MenuName { get; }
-            void OnGUI();
+            VisualElement CreateGUI()
+            {
+                return new IMGUIContainer(OnGUI);
+            }
+            void OnGUI() { }
         }
 
         internal static void RegisterMenu(ITTTMenuWindow menuWindow)
