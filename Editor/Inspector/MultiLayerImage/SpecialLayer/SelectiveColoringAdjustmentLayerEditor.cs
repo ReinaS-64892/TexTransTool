56c2b2d18ff12ad4150b8d492e603193511e97dc
tliks <aoft725@gmail.com>
2025-11-18 14:04:58 +0900

    Revert "chore: visibility of SelectiveColoringAdjustmentLayerEditor"
    This reverts commit bc8f7e80d7fd64dffc5b11d6b5f1a1aad6635ea7.

--- Editor/Inspector/MultiLayerImage/SpecialLayer/SelectiveColoringAdjustmentLayerEditor.cs
+++ Editor/Inspector/MultiLayerImage/SpecialLayer/SelectiveColoringAdjustmentLayerEditor.cs
@@ -10,29 +10,8 @@ namespace net.rs64.TexTransTool.Editor.MultiLayerImage
     [CanEditMultipleObjects]
     internal class SelectiveColoringAdjustmentLayerEditor : AbstractLayerEditor
     {
-        private static readonly GUILayoutOption[] EqualWidthOptions = { GUILayout.ExpandWidth(true), GUILayout.MinWidth(0) };
-
-        private static GUIStyle? _centeredLabelStyle;
-        private static GUIStyle CenteredLabelStyle => _centeredLabelStyle ??= new GUIStyle(EditorStyles.boldLabel) { alignment = TextAnchor.MiddleCenter };
-
-        private static readonly GUILayoutOption[] SliderOptions = { GUILayout.MinWidth(30), GUILayout.ExpandWidth(true) };
-        private static readonly GUILayoutOption[] FloatFieldOptions = { GUILayout.Width(38), GUILayout.ExpandWidth(false) };
-
-        private static float DrawCompactSliderWithField(float value, float min = -1f, float max = 1f)
-        {
-            EditorGUILayout.BeginHorizontal(EqualWidthOptions);
-            value = GUILayout.HorizontalSlider(value, min, max, SliderOptions);
-            value = Mathf.Clamp(EditorGUILayout.FloatField(value, FloatFieldOptions), min, max);
-            EditorGUILayout.EndHorizontal();
-            return value;
-        }
-
         protected override void DrawInnerProperties()
         {
-            DrawCMYKHeader();
-            
-            EditorGUILayout.Space();
-
             DrawCMYKProperty("SelectiveColoringAdjustmentLayer:label:RedsCMYK".Glc(), nameof(SelectiveColoringAdjustmentLayer.RedsCMYK));
             DrawCMYKProperty("SelectiveColoringAdjustmentLayer:label:YellowsCMYK".Glc(), nameof(SelectiveColoringAdjustmentLayer.YellowsCMYK));
             DrawCMYKProperty("SelectiveColoringAdjustmentLayer:label:GreensCMYK".Glc(), nameof(SelectiveColoringAdjustmentLayer.GreensCMYK));
@@ -49,36 +28,21 @@ namespace net.rs64.TexTransTool.Editor.MultiLayerImage
             EditorGUILayout.PropertyField(isAbsolute, "SelectiveColoringAdjustmentLayer:prop:IsAbsolute".Glc());
         }
 
-        private void DrawCMYKHeader()
-        {
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField("SelectiveColoringAdjustmentLayer:label:CyansCMYK".Glc(), CenteredLabelStyle, EqualWidthOptions);
-            EditorGUILayout.LabelField("SelectiveColoringAdjustmentLayer:label:MagentasCMYK".Glc(), CenteredLabelStyle, EqualWidthOptions);
-            EditorGUILayout.LabelField("SelectiveColoringAdjustmentLayer:label:YellowsCMYK".Glc(), CenteredLabelStyle, EqualWidthOptions);
-            EditorGUILayout.LabelField("SelectiveColoringAdjustmentLayer:label:BlacksCMYK".Glc(), CenteredLabelStyle, EqualWidthOptions);
-            EditorGUILayout.EndHorizontal();
-        }
-
         private void DrawCMYKProperty(GUIContent label, string propertyName)
         {
             var property = serializedObject.FindProperty(propertyName);
-            var vector = property.vector4Value;
-            
-            EditorGUILayout.BeginVertical("helpbox");
-            
-            EditorGUILayout.LabelField(label, EditorStyles.boldLabel);
-
-            EditorGUILayout.BeginHorizontal();
-            vector.x = DrawCompactSliderWithField(vector.x); // Cyan
-            vector.y = DrawCompactSliderWithField(vector.y); // Magenta
-            vector.z = DrawCompactSliderWithField(vector.z); // Yellow
-            vector.w = DrawCompactSliderWithField(vector.w); // Black
-            EditorGUILayout.EndHorizontal();
-            
-            property.vector4Value = vector;
-                        
-            EditorGUILayout.EndVertical();
-            EditorGUILayout.Space(2);
+            property.isExpanded = EditorGUILayout.Foldout(property.isExpanded, label);
+            if (property.isExpanded)
+            {
+                EditorGUI.indentLevel++;
+                var vector = property.vector4Value;
+                vector.x = EditorGUILayout.Slider("SelectiveColoringAdjustmentLayer:label:CyansCMYK".Glc(), vector.x, -1, 1);
+                vector.y = EditorGUILayout.Slider("SelectiveColoringAdjustmentLayer:label:MagentasCMYK".Glc(), vector.y, -1, 1);
+                vector.z = EditorGUILayout.Slider("SelectiveColoringAdjustmentLayer:label:YellowsCMYK".Glc(), vector.z, -1, 1);
+                vector.w = EditorGUILayout.Slider("SelectiveColoringAdjustmentLayer:label:BlacksCMYK".Glc(), vector.w, -1, 1);
+                property.vector4Value = vector;
+                EditorGUI.indentLevel--;
+            }
         }
     }
 }
